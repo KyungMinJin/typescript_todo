4be135248b3ac99148051bc37e11950cb76cfdb8
KyungMinJin <kyung6479@korea.ac.kr>
2020-01-19 20:15:30 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
+};
+
+export default App;
