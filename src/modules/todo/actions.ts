84fad685694b544e2c39e39b021ecdffd7c76229
진경민 <kyung6479@korea.ac.kr>
2020-07-18 23:52:15 +0900

    update all dependencies

--- src/modules/todo/actions.ts
+++ src/modules/todo/actions.ts
@@ -1,4 +1,4 @@
-import { createStandardAction, createAction } from 'typesafe-actions';
+import { createAction } from 'typesafe-actions';
 
 // 액션 type
 export const ADD_TODO = 'todos/ADD_TODO';
@@ -8,11 +8,20 @@ export const REMOVE_TODO = 'todos/REMOVE_TODO';
 let nextId = 1;
 
 // 액션 생성 함수
-export const addTodo = createAction(ADD_TODO, action => (text: string) =>
-  action({
+export const addTodo = (text: string) => ({
+  type: ADD_TODO,
+  payload: {
     id: nextId++,
-    text
-  })
-);
-export const toggleTodo = createStandardAction(TOGGLE_TODO)<number>();
-export const removeTodo = createStandardAction(REMOVE_TODO)<number>();
+    text,
+  },
+});
+
+export const toggleTodo = (id: number) => ({
+  type: TOGGLE_TODO,
+  payload: id,
+});
+
+export const removeTodo = (id: number) => ({
+  type: REMOVE_TODO,
+  payload: id,
+});
